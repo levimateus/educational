bdf1661c434c8bb6e39a73219fa772f9af77929e
levimateus <levimateus2011@gmail.com>
2018-09-01 14:29:49 -0300

    Sample configuration file

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 
 require 'core/Autoload.php';
-require_once 'src/config.php';
+require_once 'config/config.php';
 
 function buildUrl($url){
 	echo $url;
@@ -12,5 +12,10 @@ define('PROJECT_ASSETS_DIR', __DIR__.'/assets/');
 define('PROJECT_CONTROLLERS_DIR', __DIR__.'/src/controllers/');
 define('PROJECT_MODELS_DIR', __DIR__.'/src/models/');
 define('PROJECT_VIEWS_DIR', __DIR__.'/src/views/');
+
+//USER LEVELS
+define('PROJECT_STUDENT', 0);
+define('PROJECT_ADMIN', 2);
+define('PROJECT_TEACHER', 1);
 
 $bootstrap = new Core\Bootstrap();

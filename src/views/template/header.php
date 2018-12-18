1e95bb19187030b6301c03085eb49b972e45f7e7
Mateus Martins <mateus.martins@amlconsulting.com.br>
2018-12-18 11:38:46 -0200

    Removing Bootstrap files from the repository

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -4,11 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- CHANGE THE PATH IF NECESSARY  -->
-	<link rel="stylesheet" href="<?php buildUrl('/assets/css/bootstrap.min.css');?>">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="shortcut icon" href="<?php buildUrl('/assets/images/favicon.ico');?>" type="image/x-icon">
 	<title><?php echo $title; ?></title>
-
-	
-	
 </head>
 <body>
